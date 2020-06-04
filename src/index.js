7925070f381eaca397a60dc84c096e448483d5b4
J. Heasly <jheasly@earthlink.net>
2020-06-03 22:32:33 -0700

    Made it run locally.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+function Person(props) {
+    return (
+      <div className="person">
+        <h1>{props.name}</h1>
+        <p>Your Age:{props.age}</p>
+      </div>
+    );
+ }
+  
+var app = (
+<div>
+    <Person name="Max" age="28" />
+    <Person name="Manu" age="29" />
+</div>
+);
+
+// ========================================
+
+// ReactDOM.render(app, document.querySelector('#app'));
+ReactDOM.render(app, document.getElementById('root'));
